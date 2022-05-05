65b51273fa32e4d22ccf10426e2f20dd6706937d
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-05-06 03:28:56 +0600

    m

---
+++ resources/views/admin/AdminRegistration.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Registration Admin</title>
+  </head>
+  <body>
+    <h1>Register as Admin</h1>
+  </body>
+</html>
