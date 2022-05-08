3610a57fde2dc6511a341653887424b24075a5b2
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-05-09 03:37:43 +0600

    m

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::get('/home', function () {
 Route::get('/admin',[AdminController::class,'show']);
 Route::get('/admin_register',[AdminController::class,'register_admin'])->name('register_admin');
 Route::post('/admin_register',[AdminController::class,'store_admin']);
-Route::get('/login'[LoginController::class,'show_logpage'])->name('login');
+Route::get('/login',[LoginController::class,'show_logpage'])->name('login');
