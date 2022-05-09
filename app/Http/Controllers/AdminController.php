afb5f004ae7e1459989151a9b0d28fee5386d272
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-05-09 21:12:15 +0600

    m

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,20 +15,12 @@ class AdminController extends Controller
       return view('admin.AdminRegistration');
     }
     public function store_admin(Request $request){
-      //dd($request->name,$request->email,$request->password);
-        /*$this->validate(request(),[
-        'name'=>'required',
-        'email'=>'required|email',
-        'password'=>'required'
-        ]);
-        */
+
          $request->validate([
           'name'=>'required',
           'email'=>'required|email',
           'password'=>'required'
         ]);
-        //dd($validated->name,$validated->email,$validated->password);
-        //dd($request);
         $user =new User();
         $user->name = $request->name;
         $user->email=$request->email;
