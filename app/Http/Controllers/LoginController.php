2cb642188e4a87f57a5d0d15b484ff52fdb59a36
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-05-09 21:14:09 +0600

    m

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,25 +11,21 @@ class LoginController extends Controller
 
     public function show_logpage(){
       return view('login');
+
     }
     public function show_logged(Request $request){
       $credentials=$request->validate([
         'email'=>['required'],
         'password'=>['required'],
       ]);
-      //dd($request->session());
-      //$credentials = $request->only('email','password');
-      //dd($credentials);
       if(Auth::attempt($credentials)){
-        //return redirect()->route('login');
-      //  $request->session()->regenerate();
         return "logged";
       }
       return back()->withErrors([
              'email' => 'The provided credentials do not match our records.',
          ])->onlyInput('email');
 
-      
+
 
 }
 }
