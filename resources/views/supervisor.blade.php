f05a7ab5461ac877ce7e991032601f780ab8a84c
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-05-18 00:57:44 +0600

    m

---
+++ resources/views/supervisor.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>supervisor</title>
+  </head>
+  <body>
+    <h1>Supervisor Page</h1>
+  </body>
+</html>
