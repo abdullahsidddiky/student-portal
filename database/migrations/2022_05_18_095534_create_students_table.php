aae03d887288b8bef88609a7e89b31c166e26c25
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-05-18 16:28:21 +0600

    m

--- database/migrations/2022_05_18_095534_create_students_table.php
+++ database/migrations/2022_05_18_095534_create_students_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->id();
+            $table->primary('id');
+            $table->string('email')->unique();
+            $table->string('phone');
             $table->timestamps();
         });
     }
