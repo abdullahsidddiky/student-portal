7b46a62684c055b05f3f2b416f30f0799145d0f6
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-05-19 21:30:28 +0600

    m

---
+++ database/migrations/2022_05_19_152456_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('supervisor_id')->unsigned();
+            $table->foreign('supervisor_id')->references('id')->on('supervisors')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('supervisor_name');
+            $table->string('course_name ');
+            $table->string('section');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
