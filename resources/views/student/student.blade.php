d882841d35201c5b7c7a1d111be4f80fdb210ef4
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-05-23 22:06:56 +0600

    m

--- resources/views/student/student.blade.php
+++ resources/views/student/student.blade.php
@@ -6,6 +6,6 @@
   </head>
   <body>
     <h1>Student Page</h1>
-    <input type="button" name="take_course" value="Take course" onclick="window.location.href={{route('take_course')}}">
+    <input type="button" onclick="window.location.href='{{route('take_course')}}';" value="Take Course">
   </body>
 </html>
