e3dd947cc2905ce4c431c855bd6d65b49e5da41f
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-05-26 03:14:22 +0600

    m

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -1,3 +1,4 @@
+@extends('links_bootstrap')
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -5,29 +6,36 @@
     <title></title>
   </head>
   <body>
+   <div class="container">
     <h1>Course</h1>
-    <form action="/course" method="POST">
+    <form action="/course" method="POST" class="form-group">
       {{csrf_field()}}
-  <label for="subject_name">Subject Name</label>
+    <div class="form-group">
+  <label for="subject_name" >Subject Name</label>
   <input list="subject" name="subject">
   <datalist id="subject" name = "subject">
     @foreach ($subject as $subject)
     <option value="{{$subject->course_name}}"></option>
     @endforeach
   </datalist>
-  <br>
-  <label for="faculty_name">faculty Name</label>
+  </div>
+  <div class="form-group">
+  <label for="faculty_name" >Faculty Name</label>
   <input list="faculty" name="faculty">
-
   <datalist id="faculty">
     @foreach ($faculty as $faculty)
     <option value="{{$faculty->name}}"></option>
     @endforeach
   </datalist>
-  <br>
-  <label for="section">section</label>
-  <input type="text" name="section"  >
+  </div>
+  <div class="form-group">
+  <label for="section" >Section</label>
+  <input type="text" name="section">
+  </div>
+  <div class="form-group">
   <button style="cursor:pointer" type="submit" class="btn btn-primary">Submit</button>
+  </div>
 </form>
+</div>
 </body>
 </html>
