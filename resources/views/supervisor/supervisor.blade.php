9783be33b99474767800cdbcd53082319894f7c2
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-05-28 05:03:46 +0600

    m

--- resources/views/supervisor/supervisor.blade.php
+++ resources/views/supervisor/supervisor.blade.php
@@ -11,14 +11,30 @@
       $supervisor=session()->get('supervisor');
       $unique_section_course_name = session()->get('unique_section_course_name');
     @endphp
-
-    <br>
-    <br>
-    <br>
-{{$unique_section_course_name}}
    <div class="container">
     <h1>Supervisor Page</h1>
     <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Index</th>
+          <th scope="col">Course Name</th>
+          <th scope="col">Section</th>
+        </tr>
+      </thead>
+
+      @foreach ($unique_section_course_name as $value)
+
+      <tbody>
+        <tr>
+          <th scope="col"></th>
+          <td>Mark</td>
+        </tr>
+        <tr>
+          <th scope="col"></th>
+          <td>Jacob</td>
+        </tr>
+      </tbody>
+      @endforeach
 
     </table>
   </div>
