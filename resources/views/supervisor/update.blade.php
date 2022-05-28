f1039e6cec3743d3d1a2c56ae7b50b8161ad966a
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-05-29 04:11:30 +0600

    m

--- resources/views/supervisor/update.blade.php
+++ resources/views/supervisor/update.blade.php
@@ -12,6 +12,14 @@
       <h3>Student ID:{{$student->id}}</h3>
       <h3>Course Name:{{$student->course_name}}</h3>
       <h3>Section:{{$student->section}}</h3>
+      <h3>Grade:
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Select Grade</option>
+            <option value="4.0">A</option>
+            <option value="3.5">A-</option>
+            <option value="3.0">B</option>
+          </select></h3>
     </div>
+
   </body>
 </html>
